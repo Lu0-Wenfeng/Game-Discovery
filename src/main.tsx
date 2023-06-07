be29b9318940c6bd59f9c98b828ac96133c59709
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-06-07 15:10:02 -0700

    remove a duplicate platform interface

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import theme from "./theme.ts";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
